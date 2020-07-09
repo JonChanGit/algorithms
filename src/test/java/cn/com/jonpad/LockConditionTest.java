b5363fa236d7451065d4cbe940a5690c648b80c3
JonChan <759282337@ffcs>
2020-07-09 17:32:17 +0800

    Lock 进行程序调度

---
+++ src/test/java/cn/com/jonpad/LockConditionTest.java
@@ -0,0 +1,111 @@
+package cn.com.jonpad;
+
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Print {
+
+  AtomicInteger num = new AtomicInteger(1);
+  Lock lock = new ReentrantLock();
+  Condition c1 = lock.newCondition();
+  Condition c2 = lock.newCondition();
+  Condition c3 = lock.newCondition();
+
+
+  public void print5() {
+    lock.lock();
+    try {
+      while (num.get() != 1) {
+        c1.await();
+      }
+
+      System.out.println(Thread.currentThread().getName() + ": print5");
+      num.set(2);
+      c2.signal();
+
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  public void print10() {
+    lock.lock();
+    try {
+      while (num.get() != 2) {
+        c2.await();
+      }
+
+      System.out.println(Thread.currentThread().getName() + ": print10");
+
+      num.set(3);
+      c3.signal();
+
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  public void print15() {
+    lock.lock();
+    try {
+      while (num.get() != 3) {
+        c3.await();
+      }
+
+      System.out.println(Thread.currentThread().getName() + ": print15");
+
+      num.set(1);
+      c1.signal();
+
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } finally {
+      lock.unlock();
+    }
+  }
+}
+
+/**
+ * @author Jon Chan
+ * @date 2020/7/9 17:12
+ */
+public class LockConditionTest {
+  @Test
+  public void test(){
+    Print p = new Print();
+
+    new Thread(() -> {
+      for (int i = 0; i < 100; i++) {
+        p.print5();
+      }
+    }, "A").start();
+
+    new Thread(() -> {
+      for (int i = 0; i < 100; i++) {
+        p.print10();
+      }
+    }, "B").start();
+
+    new Thread(() -> {
+      for (int i = 0; i < 100; i++) {
+        p.print15();
+      }
+    }, "C").start();
+
+
+    while (Thread.activeCount() > 2) {
+      // 如果当前线程大于2(Main / GC)交出执行权
+      Thread.yield();
+    }
+
+
+  }
+}
